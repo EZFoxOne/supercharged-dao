5fdbd62bf3a899f970b17e7d509043e8483ff003
Z <139626295+julianhzhu@users.noreply.github.com>
2024-04-07 04:24:39 +0800

    fix bugs

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
 export const CHANNEL_CATEGORY = "1220447576378314877"; // The category you want the voting channels to appear in
 export const PERMISSIONED_ROLE_ID = "1215214403121848350"; //  Which role is allowed to use the bot's commands
-export const VOTING_ROLE_NAME = "Baby Dragon"; // The role that members must have to be sampled
+export const VOTING_ROLE_NAME = "Dragon Scout"; // The role that members must have to be sampled
